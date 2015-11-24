56f44313a65ecb16c14e4a492dadc043c1613dc7
Alex Swan <alex@bold-it.com>
2015-11-24 01:18:03 -0800

    no .jpg

--- routes/images.js
+++ routes/images.js
@@ -3,40 +3,45 @@ var router = express.Router();
 var im = require('gm').subClass({imageMagick:true});
 var fs = require('fs')
 
-router.get('/:prompt/:left/:right.jpg', function(req, res) {
-  im(__dirname + '/../public/images/photo-blank.jpg')
-  .resize(768,576)
-  .stroke("#000000", 1)
-  
 
-  // Prompt Shadow
-  .font("Helvetica-Bold", 30)
-  .stroke("#000000", 1)
-  .fill("#000000")
-  .drawText(0, -130, wrapText(req.params.prompt, 60), 'center')
-  
-  // Prompt
-  .font("Helvetica-Bold", 30)
-  .stroke("#68ff4e", 1)
-  .fill("#68ff4e")
-  .drawText(0, -135, wrapText(req.params.prompt, 60), 'center')
-  
-
-  // Left
-  .font("Helvetica-Narrow", 24)
-  .stroke("#000000", 1)
-  .fill("#000000")
-  .drawText(-170, 2, wrapText(req.params.left, 25), 'center')
-  
-  // Right
-  .drawText(160, -15, wrapText(req.params.right, 25), 'center')
-
-
-  .toBuffer('jpg', function(err, buffer){
-    res.contentType('image/jpg');
-    res.send(buffer);
-  });
+router.get('/:prompt/:left/:right', function(req, res){
+    makeImage(req, res);
 });
+
+function makeImage(req, res){
+    im(__dirname + '/../public/images/photo-blank.jpg')
+    .resize(768,576)
+    .stroke("#000000", 1)
+    
+
+    // Prompt Shadow
+    .font("Helvetica-Bold", 30)
+    .stroke("#000000", 1)
+    .fill("#000000")
+    .drawText(0, -130, wrapText(req.params.prompt, 60), 'center')
+    
+    // Prompt
+    .font("Helvetica-Bold", 30)
+    .stroke("#68ff4e", 1)
+    .fill("#68ff4e")
+    .drawText(0, -135, wrapText(req.params.prompt, 60), 'center')
+    
+
+    // Left
+    .font("Helvetica-Narrow", 24)
+    .stroke("#000000", 1)
+    .fill("#000000")
+    .drawText(-170, 2, wrapText(req.params.left, 25), 'center')
+    
+    // Right
+    .drawText(160, -15, wrapText(req.params.right, 25), 'center')
+
+
+    .toBuffer('jpg', function(err, buffer){
+        res.contentType('image/jpg');
+        res.send(buffer);
+    });
+}
 
 function wrapText(text, maxChars) {
         var ret = [];
