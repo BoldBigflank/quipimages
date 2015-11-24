3ebb0bf48d7a0b1a7bf186ec836c617906da8338
Alex Swan <alex@bold-it.com>
2015-11-24 00:19:45 -0800

    centered text

--- routes/images.js
+++ routes/images.js
@@ -3,14 +3,13 @@ var router = express.Router();
 var im = require('gm').subClass({imageMagick:true});
 var fs = require('fs')
 
-/* GET users listing. */
 router.get('/:prompt/:left/:right', function(req, res) {
   // res.send('respond with a resource');
   console.log("Prompt: ", req.params.prompt, "south");
   console.log("Left:   ", req.params.left);
   console.log("Right:  ", req.params.right);
   
-  console.log(__dirname)
+  console.log(__dirname);
   im(__dirname + '/../public/images/photo-blank.jpg')
   .resize(768,576)
   .stroke("#000000", 2)
@@ -20,28 +19,46 @@ router.get('/:prompt/:left/:right', function(req, res) {
   .font("arial", 24)
   .stroke("#000000", 2)
   .fill("#000000")
-  .drawText(80, 170, req.params.prompt)
+  .drawText(0, -130, req.params.prompt, 'center')
   
   // Prompt
   .font("arial", 24)
   .stroke("#68ff4e", 2)
   .fill("#68ff4e")
-  .drawText(80, 165, req.params.prompt)
+  .drawText(0, -135, req.params.prompt, 'center')
   
 
   // Left
   .stroke("#000000", 2)
   .fill("#000000")
-  .drawText(100, 290, req.params.left)
+  .drawText(-170, 2, req.params.left, 'center')
   
   // Right
-  .drawText(450, 265, req.params.right)
+  .drawText(160, -15, req.params.right, 'center')
 
 
   .toBuffer('jpg', function(err, buffer){
     res.contentType('image/jpg');
     res.send(buffer);
   });
+});
+
+router.get('/comment/:message', function(req, res) {
+    im(200, 150, "#999")
+    .stroke("#000000", 2)
+    .fill("#000000")
+    .comment("hi"+req.params.message+"|200x150")
+
+
+    .stroke("#000000", 2)
+    .fill("#000000")
+    .drawText(10, 10, "Stuff")
+    
+      
+    .toBuffer('jpg', function(err, buffer){
+        res.contentType('image/jpg');
+        res.send(buffer);
+    });
 });
 
 module.exports = router;
