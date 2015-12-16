75070f0f166ba0d35ef1275180cce270fb847880
Alex Swan <alex@bold-it.com>
2015-12-16 04:09:14 -0800

    Dynamic font size

--- routes/images.js
+++ routes/images.js
@@ -110,33 +110,34 @@ function makeImage(prompt, choices, cb){
     if(choices.length == 2){
         im(__dirname + '/../public/images/photo-centered.png')
         // .resize(768,576)
-        .stroke("#000000", .1)
+        .stroke("#000000", 0.1)
 
         // Left
         // .font("Helvetica-Narrow", 24)
-        .font(__dirname + '/../public/fonts/JustAnotherHand.ttf', 48)
+        .font(__dirname + '/../public/fonts/JustAnotherHand.ttf', fontSizeForText(choices[0])*1.5)
         .stroke("#000000", 0.1)
         .fill("#000000")
         .rotate("#000", 6)
-        .drawText(-265, 18, wrapText(choices[0], 23), 'center')
+        .drawText(-265, 18, splitText(choices[0], 18), 'center')
         
         // Right
+        .font(__dirname + '/../public/fonts/JustAnotherHand.ttf', fontSizeForText(choices[1])*1.5)
         .rotate("#000", -12)
-        .drawText(99, -111, wrapText(choices[1], 23), 'center')
+        .drawText(99, -111, splitText(choices[1], 18), 'center')
 
         .rotate("#000", 6) // Return to normal
         .crop(1024, 768, 0, 0) // Fix the black bars from rotating
 
         // Prompt Shadow
         .font(__dirname + '/../public/fonts/Arvo-Regular.ttf', 40)
-        .stroke("#000000", .1)
+        .stroke("#000000", 0.1)
         .fill("#000000")
-        .drawText(-173, -375, wrapText(prompt, 42), 'center')
+        .drawText(-173, -375, splitText(prompt, 42), 'center')
         
         // Prompt
-        .stroke("#2fb3ed", .1)
+        .stroke("#2fb3ed", 0.1)
         .fill("#2fb3ed")
-        .drawText(-173, -380, wrapText(prompt, 42), 'center')
+        .drawText(-173, -380, splitText(prompt, 42), 'center')
         
         .toBuffer('png', function(err, buffer){
             cb(buffer);
@@ -148,25 +149,24 @@ function makeImage(prompt, choices, cb){
         var image = im(__dirname + '/../public/images/arrangement-' + choices.length + '.png')
         // Originally 1024x768
         // .resize(768,576)
-        .stroke("#000000", .1)
+        .stroke("#000000", 0.1)
 
         // Write the choices
         .font(__dirname + '/../public/fonts/JustAnotherHand.ttf', 32)
-        .stroke("#000000", .1)
+        .stroke("#000000", 0.1)
         .fill("#000000");
         
         var position;
         // Left tilted choices
         image.rotate("#000", 5);
         for(var i = 0; i < choices.length; i++){
-            console.log("Checking left " + arrangement[i]);
             if( ["3", "7"].indexOf(arrangement[i]) > -1 ){
-                console.log("Found left " + arrangement[i]);
                 position = positions[arrangement[i]];
                 image
+                .font(__dirname + '/../public/fonts/JustAnotherHand.ttf', fontSizeForText(choices[i]))
                 .fill("#000000")
-                .stroke("#000000", .1)
-                .drawText(position.x, position.y, wrapText(choices[i], 23), 'center');
+                .stroke("#000000", 0.1)
+                .drawText(position.x, position.y, splitText(choices[i], 18), 'center');
             }
         }
 
@@ -174,12 +174,12 @@ function makeImage(prompt, choices, cb){
         image.rotate("#000", -10);
         for(i = 0; i < choices.length; i++){
             if(["1", "9"].indexOf(arrangement[i]) > -1 ){
-                console.log("Found right " + arrangement[i]);
                 position = positions[arrangement[i]];
                 image
+                .font(__dirname + '/../public/fonts/JustAnotherHand.ttf', fontSizeForText(choices[i]))
                 .fill("#000000")
-                .stroke("#000000", .1)
-                .drawText(position.x, position.y, wrapText(choices[i], 23), 'center');
+                .stroke("#000000", 0.1)
+                .drawText(position.x, position.y, splitText(choices[i], 18), 'center');
             }
         }
 
@@ -187,12 +187,12 @@ function makeImage(prompt, choices, cb){
         image.rotate("#000", 5);
         for(i = 0; i < choices.length; i++){
             if(["2", "4", "5", "6", "8"].indexOf(arrangement[i]) > -1 ){
-                console.log("Found center " + arrangement[i]);
                 position = positions[arrangement[i]];
                 image
+                .font(__dirname + '/../public/fonts/JustAnotherHand.ttf', fontSizeForText(choices[i]))
                 .fill("#000000")
                 .stroke("#000000", 0.1)
-                .drawText(position.x, position.y, wrapText(choices[i], 23), 'center');
+                .drawText(position.x, position.y, splitText(choices[i], 18), 'center');
             }
         }
 
@@ -200,14 +200,14 @@ function makeImage(prompt, choices, cb){
         // Prompt Shadow
         image
         .font(__dirname + '/../public/fonts/Arvo-Regular.ttf', 40)
-        .stroke("#000000", .1)
+        .stroke("#000000", 0.1)
         .fill("#000000")
-        .drawText(-145, -365, wrapText(prompt, 42), 'center')
+        .drawText(-145, -365, splitText(prompt, 42), 'center')
         
         // Prompt
-        .stroke("#2fb3ed", .1)
+        .stroke("#2fb3ed", 0.1)
         .fill("#2fb3ed")
-        .drawText(-145, -370, wrapText(prompt, 42), 'center')
+        .drawText(-145, -370, splitText(prompt, 42), 'center')
         
         .crop(1024, 768, 0, 0)
         .resize(768,576)
@@ -218,42 +218,48 @@ function makeImage(prompt, choices, cb){
     
 }
 
-
-
-function wrapText(text, maxChars) {
-    if(!text) return "";
-    var ret = [];
-    var words = text.split(/\b/);
-
-    var currentLine = '';
-    var lastWhite = '';
-    words.forEach(function(d) {
-        var prev = currentLine;
-        currentLine += lastWhite + d;
-
-        var l = currentLine.length;
-
-        if (l > maxChars) {
-            ret.push(prev.trim());
-            if(d.length > maxChars){
-                ret.push(d.substring(0, maxChars));
-                d = d.substring(maxChars);
-            }
-            currentLine = d;
-            lastWhite = '';
+function splitText(text, maxChars){
+    if( text.length >= maxChars ){
+        // Split it in two
+        var i = text.indexOf(' ', text.length/2);
+        if(i < 0) i = text.lastIndexOf(' ', text.length/2);
+        if(i < 0){
+            return text.slice(0, text.length/2) + '\n' + text.slice(text.length/2);
         } else {
-            var m = currentLine.match(/(.*)(\s+)$/);
-            lastWhite = (m && m.length === 3 && m[2]) || '';
-            currentLine = (m && m.length === 3 && m[1]) || currentLine;
+            return text.slice(0, i) + '\n' + text.slice(i+1);
         }
-    });
-
-    if (currentLine) {
-        ret.push(currentLine.trim());
     }
-
-    return ret.join("\n");
+    return text;
 }
+
+function fontSizeForText(text){
+    text = splitText(text, 18);
+    var i = text.indexOf('\n');
+    var length = ( i < 0 ) ? text.length : Math.max(i, text.length-i-1);
+    if(length < 8){
+        return 58;
+        
+    } else if (length < 13) {
+        return 50;
+
+    } else if (length < 18) {
+        return 38;
+
+    } else if (length < 23) {
+        return 28;
+
+    } else if (length < 28) {
+        return 22;
+
+    } else if (length < 38) {
+        return 18;
+
+    } else {
+        return 14;
+
+    }
+}
+
 
 
 module.exports = router;
